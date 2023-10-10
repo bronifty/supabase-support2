e8ba0f07b56c25e84f59b8da56efe8a108dd8024
bronifty <bronifty@gmail.com>
2023-10-10 00:14:55 +0000

    this

--- supabase-edge/supabase/functions/cors2/index.ts
+++ supabase-edge/supabase/functions/cors2/index.ts
@@ -6,30 +6,66 @@
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log(`Function "browser-with-cors" up and running!`)
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 
-Deno.serve(async (req) => {
-  // This is needed if you're planning to invoke your function from a browser.
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
-  }
+console.log("Hello from Functions!");
 
-  try {
-    const { name } = await req.json()
-    const data = {
-      message: `Hello ${name}!`,
-    }
+serve(async (req) => {
+        console.log(req.method)
+        if (req.method === 'OPTIONS') {
+            return new Response(
+                'ok',
+                {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "POST",
+                        "Access-Control-Expose-Headers": "Content-Length, X-JSON",
+                        "Access-Control-Allow-Headers": "apikey,X-Client-Info, Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+                    }
+                }
+            );
+        }
+        else {
+            const jsonData = await req.json();
+            const name = jsonData.name;
+            const data = {message: `Hello there ${name}!`};
+            return new Response(
+                JSON.stringify(data),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "POST",
+                        "Access-Control-Expose-Headers": "Content-Length, X-JSON",
+                        "Access-Control-Allow-Headers": "apikey,X-Client-Info, Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+                    }
+                }
+            );
+        }
+});
+// Deno.serve(async (req) => {
+//   // This is needed if you're planning to invoke your function from a browser.
+//   if (req.method === 'OPTIONS') {
+//     return new Response('ok', { headers: corsHeaders })
+//   }
 
-    return new Response(JSON.stringify(data), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-      status: 200,
-    })
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-      status: 400,
-    })
-  }
-})
+//   try {
+//     const { name } = await req.json()
+//     const data = {
+//       message: `Hello ${name}!`,
+//     }
+
+//     return new Response(JSON.stringify(data), {
+//       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+//       status: 200,
+//     })
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: error.message }), {
+//       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+//       status: 400,
+//     })
+//   }
+// })
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/cors' \
